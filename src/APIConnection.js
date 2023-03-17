a2ff8a3a1c21c35668db10848d7feba00f65563e
manalsalm <manal.almazrua@gmail.com>
2023-03-17 15:48:15 +0300

    fix issue of connection

--- src/APIConnection.js
+++ src/APIConnection.js
@@ -1,6 +1,5 @@
 
 import axios from 'axios';
-import cors from 'cors';
 
 
 const url = 'https://experimental.willow.vectara.io'
@@ -8,21 +7,23 @@ const headers = {
   'Accept': 'application/json',
   'Content-Type': 'application/json',
   'customer-id': 1448396385,
-  'x-api-key': 'zqt_VlTGYYnzy_Kq-Fb42ojC3tKr3isBcoMaQv4WKA',
-  'Allow-Control-Allow-Origin': '*',
+  'x-api-key': 'zqt_VlTGYYnzy_Kq-Fb42ojC3tKr3isBcoMaQv4WKA'
   };
 
   
 export function fetchDataCompletion() {
 
     const data = JSON.stringify({
-      "model": "text-davinci-003",
-      "prompt": "Say this is a test",
-      "max_tokens": 7,
-      "temperature": 0
-    });
+      "model": "gpt-3.5-turbo",
+      "messages": [
+          {
+              "role": "user",
+              "content": "what are the latest newa about saudi arabia"
+          }
+      ]
+  });
 
-axios.post(url+'/v1/completions', data, { headers })
+axios.post(url+'/v1/chat/completions', data, { headers })
   .then(response => {
     console.log(response.data);
   })
